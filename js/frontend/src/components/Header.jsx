0ffd5a8a2a07b08fd96fb03654320d543e580642
cassiopeelaurie <cassiopee.laurie@gmail.com>
2024-01-18 22:51:21 +0100

    reco

--- js/frontend/src/components/Header.jsx
+++ js/frontend/src/components/Header.jsx
@@ -9,9 +9,11 @@ import fragrance from "../assets/Fragrance.svg";
 export default function Header() {
   return (
     <div className="header-container">
-      <Link to="/" className="logo">
-        <img src={fragrance} alt="Logo fragrance" />
-      </Link>
+      <div className="logo-container">
+        <Link to="/" className="logo">
+          <img src={fragrance} alt="Logo fragrance" />
+        </Link>
+      </div>
       <div className="images-header">
         <img className="sea" src={Mer} alt="" />
         <img className="flower" src={Fleur} alt="" />
