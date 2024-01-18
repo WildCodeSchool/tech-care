5a5c793fd6a2fb8d23a41a7a02d6c0b8f588e7b1
Antoine Cairey <cairey.antoine@gmail.com>
2024-01-18 18:53:35 +0100

    call api from frontend

--- js/frontend/src/pages/Recommendation.jsx
+++ js/frontend/src/pages/Recommendation.jsx
@@ -1,14 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
 import Header from "../components/Header";
 import Reco from "../components/Reco";
 
 function Recommendation() {
+  const { genre, prefs } = useOutletContext();
+  const body = { genre, prefs };
   const [getReco, setGetReco] = useState([]);
   useEffect(() => {
     const getRecoUser = async () => {
       try {
-        const response = await axios.get("http://localhost:3310/api/recos");
+        const response = await axios.post(
+          "http://localhost:3310/api/recos",
+          body
+        );
         setGetReco(response.data);
       } catch (err) {
         console.error(err);
@@ -16,35 +22,19 @@ function Recommendation() {
     };
     getRecoUser();
   }, []);
+
   return (
     <div>
       <Header />
       <h1 className="selection">Votre sélection de parfum</h1>
-      <Reco
-        TitreParfum={getReco.nom}
-        MarqueParfum="Marque parfum"
-        price={getReco.prix}
-      />
-      <Reco
-        TitreParfum={getReco.nom}
-        MarqueParfum="Marque parfum"
-        price={getReco.prix}
-      />
-      <Reco
-        TitreParfum={getReco.nom}
-        MarqueParfum="Marque parfum"
-        price={getReco.prix}
-      />
-      <Reco
-        TitreParfum={getReco.nom}
-        MarqueParfum="Marque parfum"
-        price={getReco.prix}
-      />
-      <Reco
-        TitreParfum={getReco.nom}
-        MarqueParfum="Marque parfum"
-        price={getReco.prix}
-      />
+      {getReco.map((reco) => (
+        <Reco
+          nom={reco.nom}
+          marque={reco.marque}
+          prix={reco.prix}
+          image={reco.image}
+        />
+      ))}
     </div>
   );
 }
